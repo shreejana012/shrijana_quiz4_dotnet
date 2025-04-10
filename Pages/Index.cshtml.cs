ac4fa8acc25285033176c8f7e8789ea0b551664c
Shrijana Poudel <spoude14@my.centennialcollege.ca>
2025-04-10 13:48:31 -0400

    first commit

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Quiz4WebApp.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+   public void OnGet()
+    {
+        Message = "Database access has already been done on April 1, 2025 by John Doe";
+    }
+}
